762d2e8466a197031e784ea05634b056ddc4a816
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-08-21 22:07:52 -0300

    EXERCICIOS DO 1 AO 4 - LISTA 1

---
+++ 21-08/somatotal.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Primeiro exemplo de formulário</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+
+<?php
+    $numero1 = $_POST['numero1'];
+    $numero2 = $_POST['numero2'];
+    $soma = $numero1 + $numero2;
+    echo "<p> A soma do valor digitado $numero1 e do valor digitado $numero2 é: $soma </p>";
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
