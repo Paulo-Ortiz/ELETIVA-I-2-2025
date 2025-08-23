b5d79ec0d9126c2db8863c2d9abfe22c6f4848b1
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-08-22 22:00:49 -0300

    exercicio do 1 ao 7 - dia 22-08-25

---
+++ Lista_atividades_1/5_medias.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Média de 3 notas</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Média de 3 notas</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valor1" class="form-label">valor1</label>
+              <input type="number" id="valor1" name="valor1" class="form-control">
+            </div><div class="mb-3">
+              <label for="valor2" class="form-label">valor2</label>
+              <input type="number" id="valor2" name="valor2" class="form-control">
+            </div><div class="mb-3">
+              <label for="valor3" class="form-label">valor3</label>
+              <input type="number" id="valor3" name="valor3" class="form-control">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+        $valor1 = $_POST['valor1'];
+        $valor2 = $_POST['valor2'];
+        $valor3 = $_POST['valor3'];
+        $medias = (($valor1 + $valor2 + $valor3) / 3);
+        echo "<p></p>";
+        echo "<p> A média das notas informadas $valor1, $valor2 e $valor3 é: ".number_format($medias, 2)."</p>";
+    }
+?>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
