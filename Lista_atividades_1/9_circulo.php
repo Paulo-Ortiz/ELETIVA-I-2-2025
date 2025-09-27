779b272d0cea14b1cd42770afef4548cf2ccc00a
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-09-27 17:44:08 -0300

    correção

--- Lista_atividades_1/9_circulo.php
+++ Lista_atividades_1/9_circulo.php
@@ -11,24 +11,9 @@
 <h1>Calcule a área de um circulo!</h1>
 <form method="post">
 <div class="row inline-row mb-3">
-  <div class="col-md-3" bis_skin_checked="1">
-    <label for="medida" class="form-label">O valor é referente a:</label>
-    <select id="medida" name="medida" class="form-select">
-    <option value="a">Raio</option>
-    <option value="b">Diâmetro</option>
-    </select>
-  </div>
   <div class="col-md-3">
-    <label for="valor" class="form-label">Insira o valor:</label>
+    <label for="valor" class="form-label">Insira o valor do raio (centímetro):</label>
     <input type="number" id="valor" name="valor" class="form-control" required="">
-  </div>
-  <div class="col-md-3" bis_skin_checked="1">
-    <label for="unidade" class="form-label">unidade</label>
-    <select id="unidade" name="unidade" class="form-select">
-    <option value="centímetros quadrados (cm²)">Centímetros</option>
-    <option value="metros quadrados (m²)">Metros</option>
-    <option value="polegadas quadradas (pol²)">Polegadas</option>
-    </select>
   </div>
 </div>
 <button type="submit" class="btn btn-primary">Calcular</button>
@@ -36,18 +21,11 @@
 <?php
     if ($_SERVER['REQUEST_METHOD'] == 'POST') 
     {
-    $medida = $_POST['medida'];
     $valor = $_POST['valor'];
-    $unidade = $_POST['unidade'];
-      if ($medida == 'a')
       {$area = 3.14159265 * ($valor ** 2);
-        echo "  π * $valor ² = $area $unidade";
-      }
-      elseif ($medida == 'b')
-      {$area = ((3.14159265 * ($valor ** 2))/4);
-        echo " ( π * $valor ²)/4 = $area $unidade";
-      }
+        echo "O valor da área calculada é: {$area} cm²";
     }
+  }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
