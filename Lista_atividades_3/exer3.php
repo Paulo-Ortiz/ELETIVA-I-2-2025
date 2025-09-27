bcafe79744c07219a49a6c15b529cd162ea1ea69
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-09-27 17:16:08 -0300

    inclusão de cabeçários e rodapes na lista de exercícios 3

--- Lista_atividades_3/exer3.php
+++ Lista_atividades_3/exer3.php
@@ -1,46 +1,33 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 3 - Funções</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
-</head>
-<body> 
+<?php include("cabecalho.php") ?>
 <div class="container py-3">
-<h1>Exercício 3 - Funções</h1>
-<form method="post">
-<div class="mb-3">
-              <label for="palavra1" class="form-label">Digite a primeira palavra:</label>
-              <input type="text" id="palavra1" name="palavra1" class="form-control" required="">
-            </div>
-<div class="mb-3">
-              <label for="palavra2" class="form-label">Digite a segunda palavra:</label>
-              <input type="text" id="palavra2" name="palavra2" class="form-control" required="">
-            </div>
-<button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+    <h1>Exercício 3 - Funções</h1>
+    <form method="post">
+        <div class="mb-3">
+            <label for="palavra1" class="form-label">Digite a primeira palavra:</label>
+            <input type="text" id="palavra1" name="palavra1" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+            <label for="palavra2" class="form-label">Digite a segunda palavra:</label>
+            <input type="text" id="palavra2" name="palavra2" class="form-control" required="">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
 
-<?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $palavra1 = $_POST['palavra1'];
         $palavra2 = $_POST['palavra2'];
-    }
 
-        function palavraContida($palavra1, $palavra2){
-        return str_contains($palavra1, $palavra2);
-        
+        function palavraContida($palavra1, $palavra2)
+        {
+            return str_contains($palavra1, $palavra2);
+        }
+        echo "<p></p>";
+        if (palavraContida($palavra1, $palavra2)) {
+            echo "A palavra '$palavra2' está contida dentro de '$palavra1'.";
+        } else {
+            echo "A segunda palavra não está contida dentro da primeira palavra.";
+        }
     }
-    echo "<p></p>";
-    if (palavraContida($palavra1, $palavra2)) {
-        echo "A palavra '$palavra2' está contida dentro de '$palavra1'.";
-    } else {
-        echo "A segunda palavra não está contida dentro da primeira palavra.";
-    }
-?>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-</div>
-</body>
-</html>
+    include("rodape.php")
+    ?>
--- Lista_atividades_3/exer4.php
+++ Lista_atividades_3/exer4.php
@@ -1,51 +1,38 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Exercício 4 - Funções</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
-</head>
-<body> 
+<?php include("cabecalho.php") ?>
 <div class="container py-3">
-<h1>Exercício 4 - Funções</h1>
-<form method="post">
-<div class="mb-3">
-              <label for="dia" class="form-label">Digite um dia:</label>
-              <input type="text" id="dia" name="dia" class="form-control" required="">
-            </div>
-<div class="mb-3">
-              <label for="mes" class="form-label">Digite um mês:</label>
-              <input type="text" id="mes" name="mes" class="form-control" required="">
-            </div>
-<div class="mb-3">
-              <label for="ano" class="form-label">Digite um ano:</label>
-              <input type="text" id="ano" name="ano" class="form-control" required="">
-            </div>
-<button type="submit" class="btn btn-primary">Enviar</button>
-</form>
+    <h1>Exercício 4 - Funções</h1>
+    <form method="post">
+        <div class="mb-3">
+            <label for="dia" class="form-label">Digite um dia:</label>
+            <input type="text" id="dia" name="dia" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+            <label for="mes" class="form-label">Digite um mês:</label>
+            <input type="text" id="mes" name="mes" class="form-control" required="">
+        </div>
+        <div class="mb-3">
+            <label for="ano" class="form-label">Digite um ano:</label>
+            <input type="text" id="ano" name="ano" class="form-control" required="">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
 
-<?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {    
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $dia = $_POST['dia'];
         $mes = $_POST['mes'];
         $ano = $_POST['ano'];
-    }
 
-        function verificarData($mes, $dia, $ano){
-        return checkdate($mes, $dia, $ano);
-        
+        function verificarData($mes, $dia, $ano)
+        {
+            return checkdate($mes, $dia, $ano);
+        }
+        echo "<p></p>";
+        if (verificarData($mes, $dia, $ano)) {
+            echo "A data informada é válida: " . $dia . "/" . $mes . "/" . $ano;
+        } else {
+            echo "A data informada não existe.";
+        }
     }
-    echo "<p></p>";
-    if (verificarData($mes, $dia, $ano)) {
-        echo "A data informada é válida: " . $dia . "/" . $mes . "/" . $ano;
-    } else {
-        echo "A data informada não existe.";
-    }
-?>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-</div>
-</body>
-</html>
+    include("rodape.php")
+    ?>
