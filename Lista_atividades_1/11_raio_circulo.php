e77d54f0de9cbc51162f824817404b70246c204f
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-09-27 19:24:22 -0300

    raio_circulo

---
+++ Lista_atividades_1/11_raio_circulo.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title></title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container">
+<h1>Calcule o perimetro de um circulo!</h1>
+<form method="post">
+<div class="row inline-row mb-3">
+  <div class="col-md-3">
+    <label for="valor" class="form-label">Insira o valor do raio em centímetros (cm):</label>
+    <input type="number" id="valor" name="valor" class="form-control" required="">
+  </div>
+</div>
+<button type="submit" class="btn btn-primary">Calcular</button>
+</form>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') 
+    {
+    $valor = $_POST['valor'];
+      {$perimetro = 3.14159265 * $valor * 2;
+        echo " 2 x π x $valor = " . round($perimetro, 1) ." cm";
+      }
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
