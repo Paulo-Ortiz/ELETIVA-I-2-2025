a31cfd4df3d16d56aabb496d91beb6eae80fda64
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-09-28 01:46:41 -0300

    exercício 4

--- Lista_atividades_4/exer4.php
+++ Lista_atividades_4/exer4.php
@@ -1,6 +1,43 @@
 <?php include("cabecalho.php"); ?>
-<h1>shuahsuashuhu</h1>
-<?php
 
+<div class="col-lg-10 col-md-10 col-sm-12">
+    <h1>Exercicio 4</h1>
+    <form method="post">
+        <?php for ($i = 1; $i <= 5; $i++): ?>
+            <p class="fw-bold">Item <?= $i ?></p>
+            <div class="row inline-row mb-3">
+                <div class="col-md-8">
+                    <label for="nome<?= $i ?>" class="form-label">Nome</label>
+                    <input type="text" id="nome<?= $i ?>" name="nomes[]" class="form-control" required="" placeholder="Insira aqui o nome do item">
+                </div>
+                <div class="col-md-4">
+                    <label for="preco<?= $i ?>" class="form-label">Preço</label>
+                    <input type="number" id="preco<?= $i ?>" name="precos[]" class="form-control" required="" placeholder="Insira aqui o preço" step="any">
+                </div>
+            </div>
+        <?php endfor; ?>
+        <div>
+            <button type="submit" class="btn btn-success">Exibir Lista</button>
+        </div>
+</div>
+<p></p>
+</form>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nomes = $_POST['nomes'];
+    $precos = $_POST['precos'];
+    $itens = [];
+    for ($i = 0; $i < 5; $i++) {
+        $nome = $nomes[$i];
+        $preco = $precos[$i] + ($precos[$i] * 0.15);
+        $itens[$nome] = $preco;
+    }
+
+    asort($itens);
+    echo "<p class='fw-bold'>Lista de itens após 15% de imposto:</p>";
+    foreach ($itens as $nome => $preco) {
+        echo "<p>".$nome. " - R$".number_format($preco, 2, ',', '.')."<p>";
+    }
+}
 include("rodape.php");
 ?>
--- Lista_atividades_4/exer5.php
+++ Lista_atividades_4/exer5.php
@@ -1,6 +1,54 @@
 <?php include("cabecalho.php"); ?>
-<h1>shuahsuashuhu</h1>
-<?php
+<div class="col-lg-10 col-md-10 col-sm-12">
 
+    <h1>Exercicio 5</h1>
+    <p></p>
+    <form method="post">
+        <?php for ($i = 1; $i <= 5; $i++): ?>
+            <p class="text-start">LIVRO <?= $i ?></p>
+            <div class="row inline-row mb-3">
+                <div class="col-md-8">
+                    <label for="nomes<?= $i ?>" class="form-label">Título</label>
+                    <input type="text" id="nomes<?= $i ?>" name="nomes[]" class="form-control">
+                </div>
+                <div class="col-md-2">
+                    <label for="qtde<?= $i ?>" class="form-label">Qtde. em Estoque</label>
+                    <input type="number" id="qtde<?= $i ?>" name="qtde[]" class="form-control">
+                </div>
+                <p></p>
+            </div>
+        <?php endfor; ?>
+        <div>
+            <button type="submit" class="btn btn-success">Organizar</button>
+        </div>
+</div>
+<p></p>
+</form>
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nomes = $_POST['nomes'];
+    $quantidade = $_POST['qtde'];
+    $produtos = [];
+
+    for ($i = 0; $i < 5; $i++) {
+        $nome = $nomes[$i];
+        $qtd = $quantidade[$i];
+            if ($quantidade < 5) {
+                echo "O livro '{$nome}' está em baixa quantidade: {$qtd} unidades.<br>";
+            } else {
+                echo "Não existem produtos com nível de estoque baixo.";
+            }
+        }
+    }
+
+    uasort($produtos, function ($a, $b) {
+        return strcmp($a['nome'], $b['nome']);
+    });
+
+    echo "<p class='fw-bold'>Lista de produtos</p>";
+    foreach ($produtos as $codigo => $atributos) {
+        echo "<p>Código: $codigo | Nome: {$atributos['nome']} | Qtde: {$atributos['qtd']}</p>";
+
+    }
 include("rodape.php");
 ?>
