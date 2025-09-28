54fa130cb65b50245046187d0204921a8e76abb3
Paulo-Ortiz <paulo.ortiiz@hotmail.com>
2025-09-28 02:12:36 -0300

    exercicio 5

--- Lista_atividades_4/exer5.php
+++ Lista_atividades_4/exer5.php
@@ -1,54 +1,46 @@
 <?php include("cabecalho.php"); ?>
-<div class="col-lg-10 col-md-10 col-sm-12">
 
+<div class="col-lg-10 col-md-10 col-sm-12">
     <h1>Exercicio 5</h1>
-    <p></p>
     <form method="post">
         <?php for ($i = 1; $i <= 5; $i++): ?>
-            <p class="text-start">LIVRO <?= $i ?></p>
+            <p class="fw-bold">Livro <?= $i ?></p>
             <div class="row inline-row mb-3">
                 <div class="col-md-8">
-                    <label for="nomes<?= $i ?>" class="form-label">Título</label>
-                    <input type="text" id="nomes<?= $i ?>" name="nomes[]" class="form-control">
+                    <label for="nome<?= $i ?>" class="form-label">Nome do livro</label>
+                    <input type="text" id="nome<?= $i ?>" name="nomes[]" class="form-control" required="" placeholder="Insira aqui o nome do livro">
                 </div>
-                <div class="col-md-2">
-                    <label for="qtde<?= $i ?>" class="form-label">Qtde. em Estoque</label>
-                    <input type="number" id="qtde<?= $i ?>" name="qtde[]" class="form-control">
+                <div class="col-md-4">
+                    <label for="estoque<?= $i ?>" class="form-label">Quantidade em estoque</label>
+                    <input type="number" id="estoque<?= $i ?>" name="estoques[]" class="form-control" required="" placeholder="Insira aqui o estoque">
                 </div>
-                <p></p>
             </div>
         <?php endfor; ?>
         <div>
-            <button type="submit" class="btn btn-success">Organizar</button>
+            <button type="submit" class="btn btn-success">Verificar estoque</button>
         </div>
+        <p></p>
 </div>
-<p></p>
 </form>
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $nomes = $_POST['nomes'];
-    $quantidade = $_POST['qtde'];
-    $produtos = [];
-
+    $estoques = $_POST['estoques'];
+    $listaestoque = [];
     for ($i = 0; $i < 5; $i++) {
         $nome = $nomes[$i];
-        $qtd = $quantidade[$i];
-            if ($quantidade < 5) {
-                echo "O livro '{$nome}' está em baixa quantidade: {$qtd} unidades.<br>";
-            } else {
-                echo "Não existem produtos com nível de estoque baixo.";
-            }
+        $estoque = $estoques[$i];
+        $listaestoque[$nome] = $estoque;
+        if($estoque<5){
+            echo "<p class='text-danger'>O livro {$nome} está com o estoque baixo {$estoque} unidade(s).</p>";
         }
     }
 
-    uasort($produtos, function ($a, $b) {
-        return strcmp($a['nome'], $b['nome']);
-    });
-
-    echo "<p class='fw-bold'>Lista de produtos</p>";
-    foreach ($produtos as $codigo => $atributos) {
-        echo "<p>Código: $codigo | Nome: {$atributos['nome']} | Qtde: {$atributos['qtd']}</p>";
-
+    ksort($listaestoque);
+    echo "<p class='fw-bold'>Estoque de livros</p>";
+    foreach ($listaestoque as $nome => $estoque) {
+        echo "<p>Livro: {$nome} - Quantidade: {$estoque}<p>";
     }
+}
 include("rodape.php");
 ?>
